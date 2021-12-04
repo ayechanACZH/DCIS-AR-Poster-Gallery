e6fd4bb76474c5c0e62d8eb2ca5f38a76498f4ac
ayechanACZH <ayechan.aczh@gmail.com>
2021-12-04 23:09:31 +0800

    Added Gaze interaction
    When user gazes at a specific poster the relevant poster information will appear. Additionally, the poster information will always face the camera (However, I might disable this feature in the future as the text disappears inside the wall).

---
+++ DCIS AR Portal Gallery/Assets/Scripts/Gaze.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Gaze : MonoBehaviour
+{
+    List<InfoBehavior> infos = new List<InfoBehavior>();
+
+    void Start()
+    {
+        infos = FindObjectsOfType<InfoBehavior>().ToList();
+    }
+
+    void Update()
+    {
+        if(Physics.Raycast(transform.position,transform.forward, out RaycastHit hit))
+        {
+            GameObject go = hit.collider.gameObject;
+            if (go.CompareTag("hasInfo"))
+            {
+                OpenInfo(go.GetComponent<InfoBehavior>());
+                print("HERE");
+            }
+        }else
+        {
+            CloseAll();
+            print("not here");
+        }
+    }
+
+    void OpenInfo(InfoBehavior desiredInfo)
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            if(info == desiredInfo) {
+                info.OpenInfo();
+            }
+            else
+            {
+                info.CloseInfo();
+                print("not here");
+            }
+        }
+    }
+
+    void CloseAll()
+    {
+        foreach (InfoBehavior info in infos)
+        {
+            info.CloseInfo();
+            
+        }
+    }
+
+}
